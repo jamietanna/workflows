055dc6dfe63fc6fe59852c1265357b798bf0ac37
scottybollinger <scotty.bollinger@elastic.co>
2024-04-11 14:03:34 -0500

    Test fetching of config

--- .github/actions/version-content/index.js
+++ .github/actions/version-content/index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core')
 // const github = require('@actions/github')
+const fs = require('fs')
+const path = require('path')
 
 try {
   const prebuild = core.getInput('prebuild')
@@ -8,6 +10,18 @@ try {
   console.log('cwd', process.cwd())
   console.log('prebuild', prebuild)
   console.log('siteRepo', siteRepo)
+
+  let config = {}
+  const configPath = path.resolve(
+    process.cwd(),
+    siteRepo,
+    'docsmobile.config.js'
+  )
+
+  if (fs.existsSync(configPath)) {
+    config = require(configPath)
+    console.log('config', Object.keys(config))
+  }
 
 } catch (error) {
   core.setFailed(error.message)
