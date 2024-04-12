a3b03de6ba6cb6c7dd2d14a5a5727fad3cf564f9
scottybollinger <scotty.bollinger@elastic.co>
2024-04-12 14:52:33 -0500

    more debugging

--- .github/actions/version-content/index.js
+++ .github/actions/version-content/index.js
@@ -62,6 +62,7 @@ try {
   const { sources, versioning } = config
   const { directories } = sources.find(({ repo }) => repo === CONTENT_REPO)
   core.setOutput('versions', '["main", "1.0.0"]')
+  core.exportVariable('envVar', '["main", "1.0.0"]')
 
   directories.forEach(async ({ versioningSystem, path: contentPath }) => {
     allVersions = versioning[versioningSystem].all
