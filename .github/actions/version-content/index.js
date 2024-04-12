1bafb53c7812b2bed264f3cab5c6245b6f9e3ac3
scottybollinger <scotty.bollinger@elastic.co>
2024-04-12 11:40:12 -0500

    Here goes!

--- .github/actions/version-content/index.js
+++ .github/actions/version-content/index.js
@@ -1,22 +1,11 @@
 const core = require('@actions/core')
-// const github = require('@actions/github')
-const fs = require('fs')
+const fs = require('fs-extra')
+const glob = require('glob')
 const path = require('path')
 
-try {
-  const {
-    PREBUILD,
-    SITE_REPO,
-    CONTENT_REPO,
-    BASE_REF,
-    WORKSPACE
-  } = process.env
+const { PREBUILD, SITE_REPO, CONTENT_REPO, BASE_REF, WORKSPACE } = process.env
 
-  console.log('PREBUILD', PREBUILD)
-  console.log('CONTENT_REPO', CONTENT_REPO)
-  console.log('BASE_REF', BASE_REF)
-  console.log('WORKSPACE', WORKSPACE)
-
+const replaceContent = async () => {
   let config = {}
   const configPath = path.resolve(WORKSPACE, SITE_REPO, 'docsmobile.config.js')
 
@@ -32,11 +21,66 @@ try {
 
   const { directories } = sources.find(({ repo }) => repo === CONTENT_REPO)
 
-  directories.forEach(({ versioningSystem }) => {
-    console.log('version', versioning[versioningSystem].all)
+  directories.forEach(async ({ versioningSystem, path }) => {
+    if (versioning[versioningSystem].all.includes(BASE_REF)) {
+      await syncFiles(path)
+    }
   })
+}
 
+const syncFiles = async (path) => {
+  const targetDir = path.join(WORKSPACE, PREBUILD, CONTENT_REPO, BASE_REF, path)
+  const sourceDir = path.join(WORKSPACE, 'tmp', path)
 
+  const patterns = {
+    include: [
+      '*.docnav.json',
+      '*.apidocs.json',
+      '*.mdx',
+      '*.png',
+      '*.gif',
+      '*.jpg',
+      '*.svg',
+      '*.jpeg',
+      '*.webp',
+      '*.devdocs.json',
+      '*/',
+    ],
+    exclude: ['cats.mdx', 'infrastructure/ansible/systests/*'],
+  }
+
+  // Ensure target directory exists
+  await fs.ensureDir(targetDir)
+
+  // Clear target directory
+  await fs.emptyDir(targetDir)
+
+  // Process each pattern
+  for (const pattern of patterns.include) {
+    // Generate glob pattern
+    const files = glob.sync(path.join(sourceDir, pattern), { nodir: true })
+
+    for (const file of files) {
+      const targetFilePath = path.join(
+        targetDir,
+        path.relative(sourceDir, file)
+      )
+      await fs.ensureDir(path.dirname(targetFilePath)) // Ensure directory exists
+      await fs.copy(file, targetFilePath) // Copy file
+    }
+  }
+
+  // Handle exclusions after all files are copied
+  for (const pattern of patterns.exclude) {
+    const files = glob.sync(path.join(targetDir, pattern), { nodir: true })
+    for (const file of files) {
+      await fs.remove(file) // Remove excluded files
+    }
+  }
+}
+
+try {
+  await replaceContent()
 } catch (error) {
   core.setFailed(error.message)
 }
